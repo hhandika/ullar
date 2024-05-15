433bbef457a9f1b038ae3a79c752dcd81bb13276
Heru Handika <herubiolog@gmail.com>
2024-05-15 18:35:30 -0500

    Add regex to match sample name.

--- src/core/new/reads.rs
+++ src/core/new/reads.rs
@@ -2,38 +2,104 @@
 
 use std::{collections::HashMap, path::PathBuf};
 
-#[allow(dead_code)]
+use once_cell::sync::Lazy;
+use regex::Regex;
+
+use crate::{
+    helper::regex::{DESCRIPTIVE_NAME_REGEX, SIMPLE_NAME_REGEX},
+    re_capture, re_capture_dynamic, re_capture_lazy,
+};
+
+#[derive(Debug, PartialEq, Clone, Eq)]
 pub enum SampleNameFormat {
-    /// sample_R1.fastq.gz
-    /// define as format 1
-    Sample,
-    /// genus_species_R1.fastq.gz
-    /// define as format 2
-    GenusEpithet,
-    /// genus_species_locality.fastq.gz
-    /// define as format 3
-    GenusEpithetLocality,
-    /// genus_species_locality_museumNo.fastq.gz
-    /// define as format 4
-    GenusEpithetLocalityMuseumNo,
+    /// Capture sample name as a single word
+    /// at the beginning of the file name.
+    /// Consists of alphanumeric characters only.
+    /// The rest of the file name is ignored.
+    /// Example:
+    /// - sample1_1.fastq,
+    /// - sample1_2.fastq,
+    /// - sample1_singleton.fastq
+    /// - sample2_L001_R1.fastq
+    /// Use SIMPLE_NAME_REGEX pattern -> r"(^[a-zA-Z0-9]+)"
+    Simple,
+    /// Capture descriptive sample name with
+    /// multiple words separated by underscores or hyphens.
+    /// Example:
+    /// - genus_species_1.fastq,
+    /// - genus_species_2.fastq,
+    /// - genus_species_singleton.fastq
+    /// - genus_species_L001_R1.fastq
+    Descriptive,
+    /// Define custom pattern for capturing sample name
+    Custom(String),
 }
 
-#[allow(dead_code)]
-pub struct ReadAssingment<'a> {
+pub struct ReadAssignment<'a> {
     pub files: &'a [PathBuf],
+    pub name_format: &'a SampleNameFormat,
     file_map: HashMap<String, Vec<PathBuf>>,
 }
 
-#[allow(dead_code)]
-impl<'a> ReadAssingment<'a> {
-    pub fn new(files: &'a [PathBuf]) -> Self {
+impl<'a> ReadAssignment<'a> {
+    pub fn new(files: &'a [PathBuf], name_format: &'a SampleNameFormat) -> Self {
         Self {
             files,
+            name_format,
             file_map: HashMap::new(),
         }
     }
 
-    pub fn assign_reads(&mut self) {}
+    pub fn assign_reads(&mut self) {
+        let pattern = self.get_pattern();
+
+        for file in self.files {
+            let sample_name = self.get_sample_name(file, pattern);
+            self.file_map
+                .entry(sample_name)
+                .or_insert_with(Vec::new)
+                .push(file.clone());
+        }
+    }
+
+    fn get_pattern(&self) -> &'a str {
+        match self.name_format {
+            SampleNameFormat::Simple => SIMPLE_NAME_REGEX,
+            SampleNameFormat::Descriptive => DESCRIPTIVE_NAME_REGEX,
+            SampleNameFormat::Custom(pattern) => pattern,
+        }
+    }
+
+    fn get_sample_name(&self, file: &PathBuf, pattern: &str) -> String {
+        let capture = match self.name_format {
+            SampleNameFormat::Simple => re_capture_lazy!(SIMPLE_NAME_REGEX, file),
+            SampleNameFormat::Descriptive => re_capture_lazy!(DESCRIPTIVE_NAME_REGEX, file),
+            SampleNameFormat::Custom(pattern) => re_capture_dynamic!(pattern, file),
+        };
+
+        match capture {
+            Some(capture_text) => {
+                let mut capture = capture_text.to_string();
+                if self.name_format == &SampleNameFormat::Simple {
+                    self.pop_last_character(&mut capture);
+                }
+                capture
+            }
+            None => {
+                let file_name = file
+                    .file_name()
+                    .expect("Failed to get file name")
+                    .to_str()
+                    .expect("Failed to convert file name to string");
+                eprintln!("Failed to capture sample name from file: {}", file_name);
+                file_name.to_string()
+            }
+        }
+    }
+
+    fn pop_last_character(&self, sample_name: &mut String) {
+        sample_name.pop();
+    }
 }
 
 #[allow(dead_code)]
