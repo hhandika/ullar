2123f55c9a6a6b422023f6131e88255d026ee761
Heru Handika <herubiolog@gmail.com>
2024-05-15 18:35:30 -0500

    Add regex to match sample name.

--- src/helper/regex.rs
+++ src/helper/regex.rs
@@ -8,6 +8,39 @@ pub const NEXUS_REGEX: &str = r"(\.nexus|\.nex|\.nxs)$";
 pub const PHYLIP_REGEX: &str = r"(\.phylip|\.phy|\.ph)$";
 pub const PLAIN_TEXT_REGEX: &str = r"(\.txt|\.text|\.log)$";
 
+/// Regular expression for matching sample names
+/// Matches a simple name with a given pattern
+/// Examples:
+/// - sample1_R1.fastq
+/// - sample2_1.fastq.gz
+/// - sample3_R1.fastq.xz
+/// - sample1_ABCD_L4_R1_001.fastq.gz
+///
+/// Will match the following sample names:
+/// - sample1
+/// - sample2
+/// - sample3
+/// - sample1
+pub const SIMPLE_NAME_REGEX: &str = r"(^[a-zA-Z0-9]+)";
+
+/// Regular expression for matching descriptive sample names
+/// Matches a descriptive name with a given pattern
+/// Examples:
+/// - genus_species_1.fastq
+/// - genus_species_2.fastq
+/// - genus_species_singleton.fastq
+/// - genus_species_locality_R1.fastq.gz
+/// - genus_species_locality_museumNo12345_1.fastq.xz
+/// etc.
+///
+/// Will match the following sample names:
+/// - genus_species
+/// - genus_species
+/// - genus_species
+/// - genus_species_locality
+/// - genus_species_locality_museumNo12345
+pub const DESCRIPTIVE_NAME_REGEX: &str = r"^(\w+)(_|-)([a-zA-Z0-9]+)(?:_|-)"; // Modified regex
+
 /// Lazy static regex matcher
 ///
 /// Matches a file name with a given pattern
@@ -28,27 +61,50 @@ macro_rules! re_match {
 
 /// Capture sample name from file name
 #[macro_export]
-macro_rules! re_capture {
+macro_rules! re_capture_lazy {
     ($pattern: ident, $path: ident) => {{
         static RE: Lazy<Regex> =
             Lazy::new(|| Regex::new($pattern).expect("Failed to compile regex"));
+        re_capture!(RE, $path, captures)
+    }};
+}
+
+#[macro_export]
+macro_rules! re_capture_dynamic {
+    ($pattern: expr, $path: ident) => {{
+        let re = Regex::new($pattern).expect("Failed to compile regex");
+        re_capture!(re, $path, captures)
+    }};
+}
+
+#[macro_export]
+macro_rules! re_capture {
+    ($pattern: ident, $path: ident, $captures: ident) => {{
         let file_name = $path.file_stem().expect("Failed to get file name");
-        RE.captures(
+        let captures = $pattern.$captures(
             file_name
                 .to_str()
                 .expect("Failed to convert file name to string"),
-        )
+        );
+
+        match captures {
+            Some(capture_text) => {
+                let capture = capture_text.get(0).map_or("", |m| m.as_str());
+                Some(capture)
+            }
+            None => None,
+        }
     }};
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     use std::path::Path;
 
     use once_cell::sync::Lazy;
     use regex::Regex;
-
-    use crate::helper::regex::FASTQ_REGEX;
 
     #[test]
     fn test_re_match_all_fastq() {
@@ -68,5 +124,50 @@ mod tests {
             let path = Path::new(path);
             assert_eq!(re_match!(FASTQ_REGEX, path), true);
         }
+    }
+
+    #[test]
+    fn test_re_match_simple_sample() {
+        let paths: Vec<&str> = vec![
+            "sample1_R1.fastq",
+            "sample2_1.fastq.gz",
+            "sample3_R1.fastq.xz",
+            "sample4_ABCD_L4_R1_001.fastq.gz",
+        ];
+
+        let sample_names: Vec<&str> = vec!["sample1", "sample2", "sample3", "sample4"]; // Added sample name
+
+        paths.iter().enumerate().for_each(|(i, path)| {
+            let path = Path::new(path);
+            let captures = re_capture_lazy!(SIMPLE_NAME_REGEX, path).unwrap();
+            assert_eq!(captures, sample_names[i]);
+        });
+    }
+
+    #[test]
+    fn test_re_match_descriptive_sample() {
+        let paths: Vec<&str> = vec![
+            "genus_species_2.fastq",
+            "genus_species_singleton.fastq",
+            "genus_species-1.fastq.gz",
+            "genus_species_locality_2.fastq.gz",
+            "genus_species_locality_museumNo12345_1.fastq.xz",
+        ];
+
+        let sample_names: Vec<&str> = vec![
+            "genus_species",
+            "genus_species",
+            "genus_species",
+            "genus_species_locality",
+            "genus_species_locality_museumNo12345",
+        ];
+
+        paths.iter().enumerate().for_each(|(i, path)| {
+            let path = Path::new(path);
+            let captures = re_capture_lazy!(DESCRIPTIVE_NAME_REGEX, path).unwrap();
+            let mut sample_name = captures.to_string();
+            sample_name.pop();
+            assert_eq!(sample_name, sample_names[i]);
+        });
     }
 }
