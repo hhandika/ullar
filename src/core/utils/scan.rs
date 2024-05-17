415de446cf13755243e8695307abd0d2fef5d898
Heru Handika <herubiolog@gmail.com>
2024-05-17 11:34:29 -0500

    Add csv writer for reads.

--- src/core/utils/scan.rs
+++ src/core/utils/scan.rs
@@ -1,5 +1,8 @@
 //! A scan command executor
-use std::path::{Path, PathBuf};
+use std::{
+    collections::BTreeMap,
+    path::{Path, PathBuf},
+};
 
 use colored::Colorize;
 use comfy_table::Table;
@@ -8,13 +11,12 @@ use segul::helper::utils;
 use crate::{
     cli::args::ScanArgs,
     helper::{
-        files::FileFinder,
-        reads::{ReadAssignment, SampleNameFormat},
+        files::{FileFinder, CSV_EXT},
+        reads::{FastqReads, ReadAssignment, SampleNameFormat},
     },
     types::{SupportedDataTypes, SupportedFormats},
 };
 
-#[allow(dead_code)]
 pub struct ScanExecutor<'a> {
     dir: &'a Path,
     output: &'a Path,
@@ -71,23 +73,44 @@ impl<'a> ScanExecutor<'a> {
         spinner.set_message("Scanning FASTQ reads...");
         let format = SupportedFormats::Fastq;
         let files = self.find_files(&format);
-        let reads = ReadAssignment::new(&files, &self.sample_name_format).assign();
-
-        let mut table = Table::new();
-        table.set_header(vec!["Sample Name", "Read1", "Read2", "Singletons"]);
-
-        for (sample_name, reads) in reads {
-            table.add_row(vec![
-                sample_name,
-                reads.read_1.to_string(),
-                reads.read_2.unwrap_or_default(),
-                reads.singletons.unwrap_or_default(),
-            ]);
-        }
+        let records = ReadAssignment::new(&files, &self.sample_name_format).assign();
 
         spinner.finish_with_message(format!("{} Finished scanning files\n", "✔".green()));
 
-        println!("{}", table);
+        if self.is_stdout {
+            self.write_stdout(&records);
+        } else {
+            self.write_csv(&records).expect("Failed to write CSV");
+        }
+    }
+
+    fn write_stdout(&self, records: &BTreeMap<String, FastqReads>) {
+        let mut table = Table::new();
+        table.set_header(vec!["Sample Name", "Read1", "Read2", "Singletons"]);
+        for (sample_name, reads) in records {
+            table.add_row(vec![
+                sample_name,
+                &reads.read_1,
+                reads.read_2.as_ref().unwrap_or(&"".to_string()),
+                &reads.singletons.as_ref().unwrap_or(&"".to_string()),
+            ]);
+        }
+        println!("{table}");
+    }
+
+    fn write_csv(
+        &self,
+        records: &BTreeMap<String, FastqReads>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let output_path = self.output.with_extension(CSV_EXT);
+        let mut writer = csv::Writer::from_path(&output_path)?;
+        writer.write_record(&["sample_name", "read1", "read2", "singletons"])?;
+        for (sample_name, reads) in records {
+            writer.serialize((sample_name, &reads.read_1, &reads.read_2, &reads.singletons))?;
+        }
+        writer.flush()?;
+        log::info!("FASTQ reads written to: {}", output_path.display());
+        Ok(())
     }
 
     fn find_files(&self, format: &SupportedFormats) -> Vec<PathBuf> {
