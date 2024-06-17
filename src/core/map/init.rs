ab888b91b65e9d58da7ed6575ff15da3fa06139a
Heru Handika <herubiolog@gmail.com>
2024-06-17 15:01:24 -0500

    Fix windows incompatibilities.

--- src/core/map/init.rs
+++ src/core/map/init.rs
@@ -14,6 +14,7 @@ use crate::core::utils::symlinks::Symlinks;
 pub struct InitMappingConfig<'a> {
     pub input_dir: &'a Path,
     pub output_dir: &'a Path,
+    #[cfg(target_family = "unix")]
     pub phyluce: bool,
 }
 
@@ -22,6 +23,7 @@ impl Default for InitMappingConfig<'_> {
         Self {
             input_dir: Path::new(DEFAULT_ASSEMBLY_OUTPUT_DIR).as_ref(),
             output_dir: Path::new(DEFAULT_CONFIG_DIR).as_ref(),
+            #[cfg(target_family = "unix")]
             phyluce: false,
         }
     }
@@ -32,6 +34,7 @@ impl<'a> InitMappingConfig<'a> {
         Self {
             input_dir: &args.dir,
             output_dir: &args.output,
+            #[cfg(target_family = "unix")]
             phyluce: args.phyluce,
         }
     }
