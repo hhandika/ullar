79b416ccf14804b0b57adc5f996a2f54b2b34031
Heru Handika <herubiolog@gmail.com>
2024-08-12 12:36:48 -0500

    Clean unnecessary borrows and poorly formatted docs.

--- ullar/src/types/reads.rs
+++ ullar/src/types/reads.rs
@@ -53,6 +53,7 @@ pub enum SampleNameFormat {
     /// - sample1_2.fastq,
     /// - sample1_singleton.fastq
     /// - sample2_L001_R1.fastq
+    ///
     /// Use SIMPLE_NAME_REGEX pattern -> r"(^[a-zA-Z0-9]+)"
     Simple,
     /// Capture descriptive sample name with
