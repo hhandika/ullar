cccbba356f94794506aac773b7abdf30c0d633b1
Heru Handika <herubiolog@gmail.com>
2024-08-15 10:29:41 -0500

    Add last params temps

--- ullar/src/core/map/lastz.rs
+++ ullar/src/core/map/lastz.rs
@@ -1,1 +1,5 @@
 //! Map contigs to reference using LASTZ.
+pub const DEFAULT_LASTZ_PARAMS: &str = "datasets/contigs/Bunomys_chrysocomus_LSUMZ39568/contigs.fasta[multiple,nameparse=full] datasets/uce-5k-probes.fasta --nogfextend --step=20 --gap=400,30 --format=maf --strand=both > results.maf";
+
+pub const DEFAULT_LASTZ_PARAMS_2: &str =
+    "--step=20 --gap=400,30 --format=maf --strand=both > results.maf";
