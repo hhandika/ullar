a6b2eb6ee76c174cc21bba85abdda82bdc436e34
Heru Handika <herubiolog@gmail.com>
2024-08-16 18:00:23 -0500

    Fix path prompts.

--- ullar/src/core/map/mod.rs
+++ ullar/src/core/map/mod.rs
@@ -97,23 +97,23 @@ impl<'a> ContigMapping<'a> {
 
     fn log_input(&self) {
         log::info!("{}", "Input".cyan());
-        log::info!("{:18} {}", "Config:", self.config_path.display());
-        log::info!("{:18} {}", "Reference:", self.reference.display());
+        log::info!("{:18}: {}", "Config:", self.config_path.display());
+        log::info!("{:18}: {}", "Reference:", self.reference.display());
         log::info!("{:18}: {}", "Task", self.task);
         self.log_aligner_info();
     }
 
     fn log_aligner_info(&self) {
         match self.aligner {
-            Aligner::Lastz => log::info!("{:18} {}", "Aligner:", "Lastz"),
-            Aligner::Exonerate => log::info!("{:18} {}", "Aligner:", "Exonerate"),
-            Aligner::Minimap => log::info!("{:18} {}", "Aligner:", "Minimap"),
+            Aligner::Lastz => log::info!("{:18}: {}", "Aligner:", "Lastz"),
+            Aligner::Exonerate => log::info!("{:18}: {}", "Aligner:", "Exonerate"),
+            Aligner::Minimap => log::info!("{:18}: {}", "Aligner:", "Minimap"),
         }
     }
 
     fn log_output(&self, report: &[MappingData]) {
         log::info!("{}", "Output".cyan());
-        log::info!("{:18} {}", "Total contigs:", report.len());
-        log::info!("{:18} {}", "Output dir:", self.output_dir.display());
+        log::info!("{:18}: {}", "Total contigs:", report.len());
+        log::info!("{:18}: {}", "Output dir:", self.output_dir.display());
     }
 }
