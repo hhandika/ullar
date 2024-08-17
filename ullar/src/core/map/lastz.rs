ea6fb0d2df5c6cacb9d307c467de2c8b73404c4a
Heru Handika <herubiolog@gmail.com>
2024-08-17 14:22:10 -0500

    Add contig summary results.

--- ullar/src/core/map/lastz.rs
+++ ullar/src/core/map/lastz.rs
@@ -29,6 +29,9 @@ pub const LASTZ_EXE: &str = "lastz";
 /// Default output to CSV for easy reading
 pub const DEFAULT_OUTPUT_EXT: &str = "csv";
 
+const LASTZ_RESULT_DIR: &str = "lastz_results";
+const LASTZ_RESULT_SUFFIX: &str = "lastz";
+
 /// Default regex pattern to reference sequence name
 pub const DEFAULT_REFNAME_REGEX: &str = r"(?i)_p\d+$";
 
@@ -233,9 +236,9 @@ impl<'a> Lastz<'a> {
 
     fn create_output_path(&self) -> Result<PathBuf, Box<dyn Error>> {
         fs::create_dir_all(self.output_dir)?;
-        let output_filename = format!("{}_lastz", self.query.get_file_stem());
-        let output_path = self
-            .output_dir
+        let output_dir = self.output_dir.join(LASTZ_RESULT_DIR);
+        let output_filename = format!("{}_{}", self.query.get_file_stem(), LASTZ_RESULT_SUFFIX);
+        let output_path = output_dir
             .join(&output_filename)
             .with_extension(DEFAULT_OUTPUT_EXT);
         Ok(output_path)
--- ullar/src/core/map/writer.rs
+++ ullar/src/core/map/writer.rs
@@ -1,6 +1,11 @@
 /// Write results
 use core::str;
-use std::{collections::HashMap, path::Path, sync::mpsc};
+use std::{
+    collections::HashMap,
+    fs,
+    path::{Path, PathBuf},
+    sync::mpsc,
+};
 
 use bio::alphabets::dna;
 use colored::Colorize;
@@ -16,43 +21,53 @@ use segul::{
 
 use crate::helper::common;
 
-use super::reports::MappingData;
+use super::{reports::MappingData, summary::FinalMappingSummary, DEFAULT_MAPPED_CONTIG_OUTPUT_DIR};
 
-pub const DEFAULT_MAPPED_CONTIG_OUTPUT_DIR: &str = "unaligned_contigs";
+pub const DEFAULT_UNALIGN_SEQUENCE_OUTPUT_DIR: &str = "unaligned_sequences";
+pub const SUMMARY_FILE_STEM: &str = "mapping_summary";
+pub const SUMMARY_EXT: &str = "csv";
+
+pub type MappedMatrix = HashMap<String, SeqMatrix>;
 
 pub struct MappedContigWriter<'a> {
     pub mapping_data: &'a [MappingData],
     pub output_dir: &'a Path,
-    pub contig_path: &'a Path,
+    pub reference_path: &'a Path,
 }
 
 impl<'a> MappedContigWriter<'a> {
     pub fn new(
         mapping_data: &'a [MappingData],
         output_dir: &'a Path,
-        contig_path: &'a Path,
+        reference_path: &'a Path,
     ) -> Self {
         Self {
             mapping_data,
             output_dir,
-            contig_path,
+            reference_path,
         }
     }
 
     pub fn generate(&self) {
         log::info!("Filtering paralogs...");
-        let unaligned_contigs = self.map_contigs();
+        let final_matrix = self.map_contigs();
         log::info!("Writing contigs to file...");
-        self.write_sequences(unaligned_contigs);
+        self.write_sequences(&final_matrix);
+        log::info!("Writing summary to file...");
+        let summary_writer =
+            SummaryWriter::new(self.output_dir, self.reference_path, &final_matrix);
+        summary_writer.write();
     }
 
     fn map_contigs(&self) -> HashMap<String, SeqMatrix> {
-        let mut unaligned_contigs: HashMap<String, SeqMatrix> = HashMap::new();
+        // All contigs mapped to reference sequence. Key is the reference sequence name
+        // and value is a map of sample name and contig sequence.
+        let mut final_matrix: MappedMatrix = HashMap::new();
         let progress_bar = common::init_progress_bar(self.mapping_data.len() as u64);
         let (tx, rx) = mpsc::channel();
         progress_bar.set_message("Mapped contigs");
         self.mapping_data.par_iter().for_each_with(tx, |tx, data| {
-            let mut matrix: HashMap<String, SeqMatrix> = HashMap::new();
+            let mut matrix: MappedMatrix = HashMap::new();
             let (mut seq, _) =
                 SeqParser::new(&data.contig_path, &DataType::Dna).parse(&types::InputFmt::Fasta);
             data.data.iter().for_each(|(refname, contig)| {
@@ -76,20 +91,22 @@ impl<'a> MappedContigWriter<'a> {
         });
 
         rx.iter().for_each(|(_, matrix)| {
-            matrix.iter().for_each(|(refname, contigs)| {
-                if unaligned_contigs.contains_key(refname) {
-                    let seq_matrix = unaligned_contigs
-                        .get_mut(refname)
-                        .expect("Failed to get sequence matrix");
-                    seq_matrix.extend(contigs.to_owned());
-                } else {
-                    unaligned_contigs.insert(refname.to_string(), contigs.to_owned());
-                }
-            });
+            self.create_mapped_matrix(&mut final_matrix, matrix);
         });
 
         progress_bar.finish_with_message(format!("{} Contigs\n", "✔".green()));
-        unaligned_contigs
+        final_matrix
+    }
+
+    fn create_mapped_matrix(&self, final_matrix: &mut MappedMatrix, matrix: MappedMatrix) {
+        matrix.iter().for_each(|(refname, contigs)| {
+            if final_matrix.contains_key(refname) {
+                let seq_matrix = final_matrix.get_mut(refname).unwrap();
+                seq_matrix.extend(contigs.to_owned());
+            } else {
+                final_matrix.insert(refname.to_string(), contigs.to_owned());
+            }
+        });
     }
 
     fn get_sequence(&self, seq: &str, strand: char) -> String {
@@ -102,11 +119,11 @@ impl<'a> MappedContigWriter<'a> {
         }
     }
 
-    fn write_sequences(&self, unaligned_contigs: HashMap<String, SeqMatrix>) {
-        let progress_bar = common::init_progress_bar(unaligned_contigs.len() as u64);
+    fn write_sequences(&self, final_matrix: &MappedMatrix) {
+        let progress_bar = common::init_progress_bar(final_matrix.len() as u64);
         progress_bar.set_message("Contigs");
-        unaligned_contigs.par_iter().for_each(|(refname, contigs)| {
-            let output_dir = self.output_dir.join(DEFAULT_MAPPED_CONTIG_OUTPUT_DIR);
+        final_matrix.par_iter().for_each(|(refname, contigs)| {
+            let output_dir = self.output_dir.join(DEFAULT_UNALIGN_SEQUENCE_OUTPUT_DIR);
             let file_name = format!("{}.fasta", refname);
             let output_path = output_dir.join(file_name);
             let header = self.get_header(contigs.clone());
@@ -122,5 +139,51 @@ impl<'a> MappedContigWriter<'a> {
         let mut header = Header::default();
         header.from_seq_matrix(&matrix, false);
         header
+    }
+}
+
+pub struct SummaryWriter<'a> {
+    pub output_dir: &'a Path,
+    pub reference_path: &'a Path,
+    pub mapped_matrix: &'a MappedMatrix,
+}
+
+impl<'a> SummaryWriter<'a> {
+    pub fn new(
+        output_dir: &'a Path,
+        reference_path: &'a Path,
+        mapped_matrix: &'a MappedMatrix,
+    ) -> Self {
+        Self {
+            output_dir,
+            reference_path,
+            mapped_matrix,
+        }
+    }
+
+    pub fn write(&self) {
+        let progress_bar = common::init_progress_bar(self.mapped_matrix.len() as u64);
+        log::info!("Writing contig summary to file...");
+        let messages = "Contig/Loci summary";
+        progress_bar.set_message(messages);
+        let output_dir = self.create_output_path();
+        let mut writer = csv::Writer::from_path(&output_dir).expect("Failed to create csv writer");
+        self.mapped_matrix.iter().for_each(|(refname, matrix)| {
+            let mut summary = FinalMappingSummary::new(refname.to_string(), matrix.len());
+            summary.summarize_matches(&self.mapped_matrix);
+            writer
+                .serialize(summary)
+                .expect("Failed to write summary to file");
+            progress_bar.inc(1);
+        });
+        progress_bar.finish_with_message(format!("{} {}\n", "✔".green(), messages));
+    }
+
+    fn create_output_path(&self) -> PathBuf {
+        let output_dir = self.output_dir.join(DEFAULT_MAPPED_CONTIG_OUTPUT_DIR);
+        fs::create_dir_all(&output_dir).expect("Failed to create output directory");
+        output_dir
+            .join(SUMMARY_FILE_STEM)
+            .with_extension(SUMMARY_EXT)
     }
 }
