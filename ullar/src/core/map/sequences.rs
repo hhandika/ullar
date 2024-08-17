84a8e4a782afb62f9154dff30144f1c76c06f4cb
Heru Handika <herubiolog@gmail.com>
2024-08-16 19:29:42 -0500

    Fix fasta writing.

--- ullar/src/core/map/sequences.rs
+++ ullar/src/core/map/sequences.rs
@@ -1,5 +1,7 @@
+use core::str;
 use std::{collections::HashMap, path::Path, sync::mpsc};
 
+use bio::alphabets::dna;
 use colored::Colorize;
 use indexmap::IndexMap;
 use rayon::prelude::*;
@@ -51,26 +53,53 @@ impl<'a> MappedContigs<'a> {
         progress_bar.set_message("Mapped contigs");
         self.mapping_data.par_iter().for_each_with(tx, |tx, data| {
             let sample_name = self.get_sample_name(&data.contig_path);
-            let mut matrix: SeqMatrix = IndexMap::new();
+            let mut matrix: HashMap<String, SeqMatrix> = HashMap::new();
             let (mut seq, _) =
                 SeqParser::new(&data.contig_path, &DataType::Dna).parse(&types::InputFmt::Fasta);
             data.data.iter().for_each(|(refname, contig)| {
                 let data = seq
                     .get(&contig.contig_name)
                     .expect("Failed to get sequence");
-                matrix.insert(refname.to_string(), data.to_string());
-                seq.swap_remove(&contig.contig_name);
+                let sequence = self.get_sequence(&data, contig.strand);
+                if matrix.contains_key(refname) {
+                    let seq_matrix = matrix.get_mut(refname).unwrap();
+                    seq_matrix.insert(sample_name.to_string(), sequence);
+                } else {
+                    let mut seq_matrix = IndexMap::new();
+                    seq_matrix.insert(sample_name.to_string(), sequence);
+                    matrix.insert(refname.clone(), seq_matrix);
+                }
             });
+            seq.clear();
             tx.send((sample_name, matrix)).expect("Failed to send data");
             progress_bar.inc(1);
         });
 
-        rx.iter().for_each(|(sample_name, matrix)| {
-            unaligned_contigs.insert(sample_name, matrix);
+        rx.iter().for_each(|(_, matrix)| {
+            matrix.iter().for_each(|(refname, contigs)| {
+                if unaligned_contigs.contains_key(refname) {
+                    let seq_matrix = unaligned_contigs
+                        .get_mut(refname)
+                        .expect("Failed to get sequence matrix");
+                    seq_matrix.extend(contigs.to_owned());
+                } else {
+                    unaligned_contigs.insert(refname.to_string(), contigs.to_owned());
+                }
+            });
         });
 
         progress_bar.finish_with_message(format!("{} Contigs\n", "✔".green()));
         unaligned_contigs
+    }
+
+    fn get_sequence(&self, seq: &str, strand: char) -> String {
+        match strand {
+            '+' => seq.to_string(),
+            '-' => str::from_utf8(&dna::revcomp(seq.as_bytes()))
+                .expect("Failed to convert sequence to string")
+                .to_string(),
+            _ => seq.to_string(),
+        }
     }
 
     fn write_sequences(&self, unaligned_contigs: HashMap<String, SeqMatrix>) {
@@ -78,7 +107,7 @@ impl<'a> MappedContigs<'a> {
         progress_bar.set_message("Contigs");
         unaligned_contigs.par_iter().for_each(|(refname, contigs)| {
             let output_dir = self.output_dir.join(DEFAULT_MAPPED_CONTIG_OUTPUT_DIR);
-            let file_name = format!("{}", refname);
+            let file_name = format!("{}.fasta", refname);
             let output_path = output_dir.join(file_name);
             let header = self.get_header(contigs.clone());
             let mut writer = SeqWriter::new(&output_path, contigs, &header);
