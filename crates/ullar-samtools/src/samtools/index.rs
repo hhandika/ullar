0bd0242d0c664af9d5554ce6d4f010b329750cc5
Heru Handika <herubiolog@gmail.com>
2026-01-29 18:49:59 -0500

    Add samtools module.

---
+++ crates/ullar-samtools/src/samtools/index.rs
@@ -0,0 +1,40 @@
+use std::path::PathBuf;
+
+pub struct SamtoolsIndex {
+    pub bam_path: PathBuf,
+    pub output_path: PathBuf,
+}
+
+impl SamtoolsIndex {
+    pub fn new<P: AsRef<std::path::Path>>(bam_path: P) -> Self {
+        SamtoolsIndex {
+            bam_path: bam_path.as_ref().to_path_buf(),
+            output_path: PathBuf::new(),
+        }
+    }
+
+    pub fn output_path<P: AsRef<std::path::Path>>(&mut self, p: P) -> &mut Self {
+        self.output_path = p.as_ref().to_path_buf();
+        self
+    }
+
+    pub fn create_index(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let output_path = if self.output_path.as_os_str().is_empty() {
+            self.bam_path.with_extension("bai")
+        } else {
+            self.output_path.clone()
+        };
+
+        let status = std::process::Command::new("samtools")
+            .arg("index")
+            .arg(&self.bam_path)
+            .arg(&output_path)
+            .status()?;
+
+        if !status.success() {
+            return Err(format!("samtools index failed for {}", self.bam_path.display()).into());
+        }
+
+        Ok(())
+    }
+}
